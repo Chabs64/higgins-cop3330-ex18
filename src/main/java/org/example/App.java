0b2a614b5f6f1e644d8b7b772ee0c0ce0abacddf
chad <chadhiggins6@gmail.com>
2021-06-06 08:54:59 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,38 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Chad Higgins
+ */
+
+/*
+You’ll often need to determine which part of a program is run based on user input or other events.
+
+Create a program that converts temperatures from Fahrenheit to Celsius or from Celsius to Fahrenheit.
+Prompt for the starting temperature. The program should prompt for the type of conversion and then perform the conversion.
+
+The formulas are
+
+C = (F − 32) × 5 / 9
+and
+
+F = (C × 9 / 5) + 32
+Example Output
+Press C to convert from Fahrenheit to Celsius.
+Press F to convert from Celsius to Fahrenheit.
+Your choice: C
+Please enter the temperature in Fahrenheit: 32
+The temperature in Celsius is 0.
+
+Constraints
+Ensure that you allow upper or lowercase values for C and F.
+Use as few output statements as possible and avoid repeating output strings.
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
